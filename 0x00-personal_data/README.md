790a0430d4e0bcd4ef9e097e5296efdb615650c0
Denis <denisstillwaters@gmail.com>
2024-02-09 11:31:16 -0800

    0x00 personal data

--- 0x00-personal_data/README.md
+++ 0x00-personal_data/README.md
@@ -1,1 +1,1 @@
-0x00-personal_data
+0x00 personal data
--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env python3
-""" Returns a salted, hashed password, byte in string """
+"""Module for encrypting passwords.
+"""
+
+
+import logging
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ Returns byte string password """
+    """Hashes the provided password using bcrypt.
+
+    Use the bcrypt package to perform the hashing (with hashpw).
+
+    Args:
+        password (str): Password to be hashed.
+
+    Returns:
+        bytes: A salted, hashed password in byte string format.
+    """
+    # Salt and hash the password using the bcrypt package
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ Implement is_valid to validate provided password
-    matched hashed_password
+    """Validates that the provided password matches the hashed password.
+
+    Args:
+        hashed_password (bytes): Hashed password.
+        password (str): Password to be validated.
+
+    Returns:
+        bool: True if the hashed password was formed from the given password,
+        otherwise False.
     """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    # Try to match the hashed password with the given password
+    try:
+        return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    # If there is an exception in the process, log the error message
+    except Exception as e:
+        logging.error("Error in password validation: {}".format(e))
+        return False
